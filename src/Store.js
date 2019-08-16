0e18f7ae56ca8b1aaf7bdc9273bc6d767ccdd82a
ekinalcar <ekinalcar@firmasoft.com>
2019-08-16 09:27:15 +0300

    package json last format

--- src/Store.js
+++ src/Store.js
@@ -2,9 +2,7 @@ import { Platform } from 'react-native';
 import {
     createStore,
     applyMiddleware,
-    compose
 } from 'redux';
-import devTools from 'remote-redux-devtools';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
@@ -13,16 +11,6 @@ import RootReducer from './Reducers';
 
 const middleware = applyMiddleware(thunk, promise, logger);
 
-const Store = createStore(
-    RootReducer,
-    compose(
-        middleware,
-        devTools({
-            name: Platform.OS,
-            hostname: 'localhost',
-            port: 5678
-        }),
-    )
-);
+const Store = createStore(RootReducer,middleware);
 
 export default Store;
